c1943694de222440cdac235dc38f43d47a19bc33
muxwebflow <muxwebflow@gmail.com>
2025-03-20 08:33:03 +0100

    Create sendToCRM.js

---
+++ netlify/functions/sendToCRM.js
@@ -0,0 +1,7 @@
+// netlify/functions/sendToCRM.js
+exports.handler = async (event, context) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: "Hello from Netlify Functions!" })
+  };
+};
